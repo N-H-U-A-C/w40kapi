fde15c9f63481694614a59ee031cb1b1490f1443
N.H.U.A.C <presenteisme.pb@gmail.com>
2023-12-07 22:07:17 +0100

    initial commit

---
+++ src/main/java/dev/cb/w40kapi/W40kapiApplication.java
@@ -0,0 +1,13 @@
+package dev.cb.w40kapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W40kapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W40kapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cb/w40kapi/W40kapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cb.w40kapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W40kapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
