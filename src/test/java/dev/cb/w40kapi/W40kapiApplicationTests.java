632344a8c88c73bb511996076a18184a93a2f709
N.H.U.A.C <presenteisme.pb@gmail.com>
2023-12-10 14:51:29 +0100

    0002, test: add web app context for testing

--- src/test/java/dev/cb/w40kapi/W40kapiApplicationTests.java
+++ src/test/java/dev/cb/w40kapi/W40kapiApplicationTests.java
@@ -3,7 +3,7 @@ package dev.cb.w40kapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class W40kapiApplicationTests {
 
 	@Test
