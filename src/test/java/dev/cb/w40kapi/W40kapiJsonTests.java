1c3658cf84b0bed61cccc144315bbab0bf64acfe
N.H.U.A.C <presenteisme.pb@gmail.com>
2023-12-11 23:15:13 +0100

    0002, test: write missing tests (json array) for Category serialization/deserialization

--- src/test/java/dev/cb/w40kapi/W40kapiJsonTests.java
+++ src/test/java/dev/cb/w40kapi/W40kapiJsonTests.java
@@ -1,6 +1,7 @@
 package dev.cb.w40kapi;
 
 import dev.cb.w40kapi.business.domain.Category;
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -15,13 +16,16 @@ class W40kapiJsonTests {
 
     @Autowired
     private JacksonTester<Category> json;
+    @Autowired
+    private JacksonTester<Category[]> jsonArray;
+
+    Category category = new Category(33, "Dummy");
+    Category[] categoryArray = Arrays.array(new Category(55,"Test"),new Category(1,"Ok"));
 
     @Test
     void CategorySerializationTest() throws IOException {
-        var category = new Category(33, "Dummy");
-
         // testing equality
-        assertThat(json.write(category)).isStrictlyEqualToJson("expected_category.json");
+        assertThat(json.write(category)).isStrictlyEqualToJson("json/category.json");
 
         // testing json values
         assertThat(json.write(category)).hasJsonPathNumberValue("@.id");
@@ -33,7 +37,7 @@ class W40kapiJsonTests {
 
     @Test
     void CategoryDeserializationTest() throws IOException {
-        String expectedCategory = """
+        String categoryJson = """
                 {
                   "id": 33,
                   "name": "Dummy"
@@ -41,12 +45,29 @@ class W40kapiJsonTests {
                 """;
 
         // testing equality
-        assertThat(json.parse(expectedCategory)).isEqualTo(new Category(33, "Dummy"));
+        assertThat(json.parse(categoryJson)).isEqualTo(category);
 
         // testing object values
-        assertThat(json.parseObject(expectedCategory).id()).isEqualTo(33);
-        assertThat(json.parseObject(expectedCategory).name()).isEqualTo("Dummy");
+        assertThat(json.parseObject(categoryJson).id()).isEqualTo(33);
+        assertThat(json.parseObject(categoryJson).name()).isEqualTo("Dummy");
     }
 
+    @Test
+    void CategoryArraySerializationTest() throws IOException {
+        // testing equality
+        assertThat(jsonArray.write(categoryArray)).isStrictlyEqualToJson("json/category_array.json");
+    }
 
+    @Test
+    void CategoryArrayDeserializationTest() throws IOException {
+        String categoryArrayJson = """
+                [
+                  {"id":  55, "name": "Test"},
+                  {"id":  1, "name": "Ok"}
+                ]
+                """;
+
+        // testing equality
+        assertThat(jsonArray.parse(categoryArrayJson)).isEqualTo(categoryArray);
+    }
 }
