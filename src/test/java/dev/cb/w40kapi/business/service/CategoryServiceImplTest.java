c6562788a321bc598aa60dab307004564b547061
N.H.U.A.C <presenteisme.pb@gmail.com>
2024-03-20 10:14:52 +0100

    0002, test: refactor pageRequest for CategoryServiceImplTest (externalize pageRequest initialization in setUp()

--- src/test/java/dev/cb/w40kapi/business/service/CategoryServiceImplTest.java
+++ src/test/java/dev/cb/w40kapi/business/service/CategoryServiceImplTest.java
@@ -1,12 +1,18 @@
 package dev.cb.w40kapi.business.service;
 
+import dev.cb.w40kapi.business.domain.Category;
 import dev.cb.w40kapi.persistence.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 import static org.mockito.Mockito.verify;
 
@@ -17,8 +23,13 @@ class CategoryServiceImplTest {
     private CategoryServiceImpl classUnderTest;
     @Mock
     private CategoryRepository categoryRepository;
-    @Mock
+
     private PageRequest pageRequest;
+
+    @BeforeEach
+    void setUp() {
+        pageRequest = PageRequest.of(0,20, Sort.by(Sort.Direction.ASC,"name"));
+    }
 
     @Test
     public void shouldCallFindAllOfCategoryRepository() {
