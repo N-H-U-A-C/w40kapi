d09eac291b5c9cee20d529a342e56bb5f41ce802
N.H.U.A.C <presenteisme.pb@gmail.com>
2024-03-30 11:29:03 +0100

    0004, feat: create SourceService interface

---
+++ src/main/java/dev/cb/w40kapi/business/service/SourceService.java
@@ -0,0 +1,22 @@
+package dev.cb.w40kapi.business.service;
+
+import dev.cb.w40kapi.business.domain.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * Interface for business logic for {@link dev.cb.w40kapi.business.domain.Source}.
+ *
+ * @author N.H.U.A.C
+ * @version 1.0
+ */
+public interface SourceService {
+
+    /**
+     * Returns a {@link Page} of {@link dev.cb.w40kapi.business.domain.Source} meeting the paging restriction provided in the {@link Pageable} object.
+     *
+     * @param pageable the {@code Pageable} to request a paged result.
+     * @return a {@code Page} of {@code Source}.
+     */
+    Page<Category> getAll(Pageable pageable);
+}
