ef662092933503d77623c3257e516a7ea607c25b
N.H.U.A.C <presenteisme.pb@gmail.com>
2024-04-04 23:56:54 +0200

    0007, feat: add @ManyToMany and @JsonIgnore annotations to Title

--- src/main/java/dev/cb/w40kapi/business/domain/Title.java
+++ src/main/java/dev/cb/w40kapi/business/domain/Title.java
@@ -1,7 +1,10 @@
 package dev.cb.w40kapi.business.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -21,6 +24,13 @@ public class Title {
     @Column(name = "NAME_TITLE")
     private String name;
 
+    @ManyToMany
+    @JoinTable(name = "DESIGNATE",
+            joinColumns = @JoinColumn(name = "ID_TITLE"),
+            inverseJoinColumns = @JoinColumn(name = "ID_AUTHOR"))
+    @JsonIgnore
+    private List<Author> authors = new ArrayList<>();
+
     /**
      * Creates a new {@code Title}.
      */
@@ -38,6 +48,19 @@ public class Title {
         this.name = name;
     }
 
+    /**
+     * Creates a new {@code Title}.
+     *
+     * @param id      the id used for the initialization.
+     * @param name    the name used for the initialization.
+     * @param authors the list of authors used for the initialization.
+     */
+    public Title(Integer id, String name, List<Author> authors) {
+        this.id = id;
+        this.name = name;
+        this.authors = authors;
+    }
+
     public Integer getId() {
         return id;
     }
@@ -46,16 +69,20 @@ public class Title {
         return name;
     }
 
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Title title = (Title) o;
-        return Objects.equals(getId(), title.getId()) && Objects.equals(getName(), title.getName());
+        return Objects.equals(getId(), title.getId()) && Objects.equals(getName(), title.getName()) && Objects.equals(authors, title.authors);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getName());
+        return Objects.hash(getId(), getName(), authors);
     }
 }
