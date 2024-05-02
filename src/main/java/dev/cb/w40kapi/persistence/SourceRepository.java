2c094289b5d407db4cb730a679683ebbc5651453
N.H.U.A.C <presenteisme.pb@gmail.com>
2024-05-02 12:28:40 +0200

    0014, feat: add findAllSourceDto method to SourceRepository

--- src/main/java/dev/cb/w40kapi/persistence/SourceRepository.java
+++ src/main/java/dev/cb/w40kapi/persistence/SourceRepository.java
@@ -1,6 +1,11 @@
 package dev.cb.w40kapi.persistence;
 
 import dev.cb.w40kapi.business.domain.Source;
+import dev.cb.w40kapi.business.service.dto.CategoryDto;
+import dev.cb.w40kapi.business.service.dto.SourceDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -11,4 +16,20 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * @version 1.0
  */
 public interface SourceRepository extends CrudRepository<Source, Integer>, PagingAndSortingRepository<Source, Integer> {
+
+    /**
+     * Returns a {@link Slice} of {@link CategoryDto} meeting the paging restriction provided in the {@link Pageable} object.
+     *
+     * @param pageable the {@code Pageable} to request a sliced result.
+     * @return a {@code Slice} of {@code CategoryDto}.
+     */
+    @Query("""
+            SELECT new dev.cb.w40kapi.business.service.dto.SourceDto(
+                s.id as id,
+                s.name as name,
+                s.publication as publication
+            )
+            FROM Source s
+            """)
+    Slice<SourceDto> findAllSourceDto(Pageable pageable);
 }
